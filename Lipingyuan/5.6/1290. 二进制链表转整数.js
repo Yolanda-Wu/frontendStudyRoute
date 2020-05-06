e4398dcc9720b6ea3593d810b425b6476b5598f8
AEIP源 <1432977406@qq.com>
2020-05-06 13:53:16 +0800

    5.6leetcode打卡完成

---
+++ Lipingyuan/5.6/1290. 二进制链表转整数.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function (head) {
+    let res = 0;
+    while (head) {
+        res = res * 2 + head.val;
+        head = head.next;
+    }
+    return res;
+};
